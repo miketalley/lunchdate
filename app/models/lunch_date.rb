1fe1288a8e08de1385f4a1b20481b8dbcff7dffb
Mike Talley <miketalley@hotmail.com>
2014-05-21 16:41:44 -0400

    Fixed a couple of bugs in my_matches view

--- app/models/lunch_date.rb
+++ app/models/lunch_date.rb
@@ -23,4 +23,9 @@ class LunchDate < ActiveRecord::Base
     end
   end
 
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
 end
