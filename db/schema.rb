7f930900b83a396de3313e8aeb77dcb763537478
Mike Talley <miketalley@hotmail.com>
2014-05-21 19:25:35 -0400

    Created partial for my_matches cycling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520142618) do
+ActiveRecord::Schema.define(version: 20140521231250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140520142618) do
     t.datetime "date_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comment"
   end
 
   create_table "matches", force: true do |t|
