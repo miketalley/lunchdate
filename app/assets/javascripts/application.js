154805eb4df83fa530e0fd5f2d06547b608cc3c0
Mike Talley <miketalley@hotmail.com>
2014-05-22 09:24:06 -0400

    Installed foundation and modified layout file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require underscore
 //= require gmaps/google
 //= require_tree .
 
+
+$(function(){ $(document).foundation(); });
